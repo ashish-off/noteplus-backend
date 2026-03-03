8c915947b505e4ec211fce6dedcababb734a1ca4
Ashish <ashishsaud498@gmail.com>
2026-03-03 23:47:55 +0545

    feat: Add logout route to authentication API

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/auth.controller";
+import { registerUser, loginUser, logoutUser } from "../controllers/auth.controller";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post
 
 export default router;
