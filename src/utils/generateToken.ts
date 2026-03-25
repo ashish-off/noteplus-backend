36a839b76bc91494533b7f1a96354aa20550b2b2
Ashish <ashishsaud498@gmail.com>
2026-03-26 00:14:17 +0545

    feat: Extend JWT token expiration to 15 days

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -4,5 +4,5 @@ export const generateToken = (id: string) => {
   const secret = process.env.JWT_SECRET;
   if (!secret) throw new Error("JWT_SECRET is not defined");
 
-  return jwt.sign({ id }, secret, { expiresIn: "1d" });
+  return jwt.sign({ id }, secret, { expiresIn: "15d" });
 };
